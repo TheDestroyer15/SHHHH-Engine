bd2468d71de8b32a1bc04fa4bc0ef3d302bb0c77
burner792wxyz <burner792wxyz@gmail.com>
2025-11-07 20:25:09 -0600

    first commit

---
+++ Maple Editor/source/scripts/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from views import *
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.route('/')
+def home():
+    return views.home()
+
+
+if __name__ == '__main__':
+    port = 5000
+
+    import socket
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    print(f'Server started on {hostname} at {ip_address}:{port}')
+
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+ 
